823e632fd4756a3d46b495e6540160b77b18ac7a
RoyaAmirii <roya.amiri@ada.ac.uk>
2024-10-23 09:47:47 +0000

    2 tests passed

--- dfp.js
+++ dfp.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
 
-function parseFile (indata, outdata, delimiter = ';') {
-
+function parseFile (indata, outdata, delimiter = ';') { 
+  if (!fs.existsSync (indata)) {//'existsSync' checks if input file exists
+    return -1; //this returns -1 if input file does not exist
+  }
+if (fs.existsSync(outdata)) {
+  fs.unlinkSync(outdata); // if output file already exists, its deleted using fs.unlinksync()
+}
+const data = fs.readFileSync(indata, 'utf-8') //input file is being read using utf encoding
+const lines = data.split('\n');//input data split into array of lines to process each row
+const transformedLines = [];
+let totalRecordsExported = 0;
 }
 
 
@@ -10,7 +19,6 @@ function parseFile (indata, outdata, delimiter = ';') {
 
 
 
-// Leave this code here for the automated tests
 module.exports = {
   parseFile,
 }
